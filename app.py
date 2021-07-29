ca8e280d4cfabb4b2091d943c44f36e0c76f2589
ElDonatello <aminebechar96@gmail.com>
2021-07-29 15:23:17 +0100

    changes1198

--- app.py
+++ app.py
@@ -140,6 +140,129 @@ def hello_world():
     val = str((np.e ** value)[0])
     return val
 
+@app.route('/light', methods=['GET', 'POST'])
+def hello_world2():
+    client = pymongo.MongoClient(
+        'mongodb+srv://amine:testtest@cluster0.qlxh6.mongodb.net/test2?retryWrites=true&w=majority')
+    db = client.test2
+    collection_sell = db['Sell']
+    sell = pd.DataFrame(list(collection_sell.find({}, {'_id': False})))
+    sell.set_index('id', inplace=True)
+
+    # In[3]:
+
+    sell_predict = sell.loc[:,['model','brand','notes','proDate','energie','transmission','kilometrage','price','location_wilaya']]
+    # In[4]:
+
+    var_model_highend = 0
+    var_model_medium = 0
+    var_energie_essence = 0
+    var_energie_gpl = 0
+    var_transmission_manuelle = 0
+    var_transmission_semi = 0
+
+    # In[5]:
+
+    filename = os.path.join(app.root_path, 'static', 'finalizeD.sav')
+    filename2 = os.path.join(app.root_path, 'static', 'finalizeD2.sav')
+    filename3 = os.path.join(app.root_path, 'static', 'finalizeD3.sav')
+    filename4 = os.path.join(app.root_path, 'static', 'finalizeD4.sav')
+    filename5 = os.path.join(app.root_path, 'static', 'finalizeD5.sav')
+    linear = os.path.join(app.root_path, 'static', 'linear5.sav')
+
+    # In[6]:
+
+    label_enc = pickle.load(open(filename, 'rb'))
+    label_enc2 = pickle.load(open(filename2, 'rb'))
+    label_enc3 = pickle.load(open(filename3, 'rb'))
+    label_enc4 = pickle.load(open(filename4, 'rb'))
+    label_enc5 = pickle.load(open(filename5, 'rb'))
+
+
+    # In[7]:
+
+    model = request.json['model']
+    brand = request.json['brand']
+    notes = request.json['notes']
+    proDate = request.json['proDate']
+    energie = request.json['energie']
+    location = request.json['location']
+    transmission = request.json['transmission']
+    kilometrage = request.json['kilometrage']
+
+    sell_predict['id'] = sell_predict.index
+    car_detaille = sell_predict[
+        (sell_predict.brand == brand) & (sell_predict.notes == notes) & (sell_predict.proDate == proDate)].groupby(
+        ['brand', 'notes', 'proDate']).describe()
+    sell_predict.set_index('id', inplace=True)
+    prix_max = car_detaille.loc[:, 'price'].iloc[0, 1]
+
+    # In[8]:
+
+    brand_num = label_enc.transform([brand])[0]
+    notes_num = label_enc2.transform([notes])[0]
+    model_num = label_enc4.transform([model])[0]
+    location_num = label_enc5.transform([location])[0]
+
+
+
+    # In[9]:
+
+    linear = pickle.load(open(linear, 'rb'))
+
+    # In[10]:
+
+    sell_predict.reset_index(inplace=True)
+    sell_predict['price'] = sell_predict['price'].astype('int')
+    temp = sell_predict.copy()
+    table = temp.groupby(['model'])['price'].mean()
+    temp = temp.merge(table.reset_index(), how='left', on='model')
+    bins = [0, 150, 400, 800]
+    cars_bin = ['Budget', 'Medium', 'Highend']
+    sell_predict['carsrange'] = pd.cut(temp['price_y'], bins, right=False, labels=cars_bin)
+    model_binaire = sell_predict[sell_predict.model == model].loc[:, 'carsrange'].iloc[1]
+
+    # In[11]:
+
+    if model_binaire == 'Highend':
+        var_model_highend = 1
+    if model_binaire == 'Medium':
+        var_model_medium = 1
+
+    # In[ ]:
+
+    # In[15]:
+
+    if transmission == 'Manuelle':
+        var_transmission_manuelle = 1
+    if transmission == 'Semi Automatique':
+        var_transmission_semi = 1
+
+    # In[ ]:
+
+    # In[16]:
+
+    if energie == 'Essence':
+        var_energie_essence = 1
+    if energie == 'GPL':
+        var_energie_gpl = 1
+    value1 = np.array([[model_num, brand_num, notes_num, proDate,location_num,  kilometrage,
+                        var_model_medium, var_model_highend,
+                       var_transmission_manuelle, var_transmission_semi, var_energie_essence, var_energie_gpl,
+                       prix_max]])
+
+    arrays = pd.DataFrame(data=value1, columns=['model', 'brand', 'notes', 'proDate',
+                                                'location','kilometrage', 'Medium', 'Highend', 'Manuelle',
+                                               'Semi Automatique', 'Essence', 'GPL', 'prix_max'])
+
+
+    value = linear.predict(arrays)
+
+    # In[19]:
+    val = str((np.e ** value)[0])
+    return val
+
+
 
 if __name__ == '__main__':
     app.run()
